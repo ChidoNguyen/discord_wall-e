1486bba7859bdd16528d61ccb40341f3370b376b
Chido Nguyen <cheeds@Chidos-MacBook-Air.local>
2025-03-09 13:53:41 -0700

    need to fix expiry time mockup for cookies testing

--- tests/test_bot_site_cookies.py
+++ tests/test_bot_site_cookies.py
@@ -1,4 +1,4 @@
-import pytest,os
+import pytest,os,time
 from unittest.mock import patch , mock_open
 from src.automation.bot_site_cookies import COOKIES, COOKIES_PATH, valid_cookies, load_cookies, save_cookies
 
@@ -24,8 +24,22 @@ def test_valid_cookies_exists(mock_exists, mock_open):
     assert results == True
     mock_open.assert_called_once_with(f'{COOKIES_PATH}/{COOKIES['fname']}','r')
 
-#opening fails
-def test_valid_cookies_none():
-    return 
+#opening fails file does not exist
+@patch("builtins.open")
+@patch("os.path.exists" , return_value = False)
+def test_valid_cookies_none(mock_exists, mock_open):
+    result = valid_cookies()
+    assert result == False
+    mock_open.assert_called_once_with(f'{COOKIES_PATH}/{COOKIES['fname']}','r')
+    
+#opening works but expired cookies
 
+@patch("builtins.open" , new_callable= mock_open, read_data = '[{"cookies": "values" , "expiry" : 1111}]')
+@patch("os.path.exists" , return_value = True)
+@patch("time.time", return_value = 99999999999)
+def test_valid_cookies_expired(mock_time,mock_exists, mock_open):
+    result = valid_cookies()
+
+    assert result == False
+    mock_open.assert_called_once_with(f'{COOKIES_PATH}/{COOKIES['fname']}' , 'r')
     
