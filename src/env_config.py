ea970d2043dbfa2f3e8366d05b74a4b2688bee29
Chido Nguyen <cheeds@Chidos-MacBook-Air.local>
2025-05-14 13:58:03 -0700

    cleaning up and converting env variables in automation scripts

--- src/env_config.py
+++ src/env_config.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import Optional
 from dotenv import load_dotenv
 
 #load .env file
@@ -13,6 +14,18 @@ class Config:
     '''
     Loads .env file and variables into one class
     '''
+    #### Type Hinting/Autocomplete Assist ###
+    DISCORD_TOKEN: Optional[str] = None
+    ADMIN_ID: Optional[str] = None
+    JANITORS: Optional[str] = None
+    PERSONAL_TEST: Optional[str] = None
+    DOWNLOAD_DIR: Optional[str] = None
+    DB_PATH: Optional[str] = None
+    THE_VAULT: Optional[str] = None
+    THE_JOBS: Optional[str] = None
+    API_ENDPOINT: Optional[str] = None
+    THE_GOODS: Optional[str] = None
+    #########################################
     REQUIRED_ENV_VARS = [
         'DISCORD_TOKEN', 'ADMIN_ID', 'JANITORS', 'PERSONAL_TEST',
         'DOWNLOAD_DIR', 'DB_PATH', 'THE_VAULT', 'THE_JOBS',
