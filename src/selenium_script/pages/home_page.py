9d30675ac55697225abade61e4eb48e733725e40
Chido Nguyen <chidonguyen@hotmail.com>
2025-05-27 20:05:50 -0700

    refactored homepage outward facing function for better error tracking

--- src/selenium_script/pages/home_page.py
+++ src/selenium_script/pages/home_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.chrome.webdriver import WebDriver as ChromeWebdriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
@@ -14,7 +15,45 @@ class HomePage:
         ''' Checks if current page is home page '''
         return config.TARGET_TITLE in self.driver.title
     
+    def get_login_container(self) -> WebElement:
+        container_class_name = 'user-data__sign'
+        try:
+            return self.driver.find_element(By.CLASS_NAME, container_class_name)
+        except NoSuchElementException as e:
+            raise LoginElementNotFound(
+                message="Could not locate login container.",
+                action="find_element by CLASS_NAME",
+                selector=f"{container_class_name}"
+            ) from e
+    
+    def get_login_anchor(self, login_container: WebElement) -> WebElement:
+        anchor_tag = 'a'
+        try:
+            return login_container.find_element(By.TAG_NAME, anchor_tag)
+        except NoSuchElementException as e:
+            raise LoginElementNotFound(
+                message= "Could not locate anchor in login container.",
+                action='find_element by TAG_NAME',
+                selector='a'
+            ) from e
+    
+    def get_login_href(self, login_anchor_element: WebElement) -> str:
+        href_url = login_anchor_element.get_attribute('href')
+        if not href_url:
+            raise LoginElementNotFound(
+                message= "Missing or empty attribute 'href' .",
+                action= "get_attribute",
+                selector= "href"
+            )
+        return href_url
+        
+        
     def get_login_url(self) -> str:
+        login_div = self.get_login_container()
+        login_anchor = self.get_login_anchor(login_div)
+        return self.get_login_href(login_anchor)
+
+    def _depecrated_get_login_url(self) -> str:
         ''' 
             Extracts the url that will redirect to login page.
             Avoid's having to handle clicking an element to trigger a modal pop-up for login.
