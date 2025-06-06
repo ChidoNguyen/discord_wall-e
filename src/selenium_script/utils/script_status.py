3e6aa0eb454f16c65ae2b7433c2b0fcc3a635aa7
Chido Nguyen <chidonguyen@hotmail.com>
2025-06-06 11:31:53 -0700

    removed singleton since we no longer use the script via CLI

--- src/selenium_script/utils/script_status.py
+++ src/selenium_script/utils/script_status.py
@@ -1,23 +1,18 @@
 #singleton approach + global var
 import json
 class BookScriptStatus:
-    _instance = None
     valid_keys: set
     output: dict
 
-    def __new__(cls): #new is the box #init fills it
-        #create itself
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance.valid_keys = {'status','metadata','steps','misc'}
-            cls._instance.output = {
-                'status' : 'fail',
-                'metadata' : None,
-                'steps' : [],
-                'message' : None,
-                'misc' : []
-            }
-        return cls._instance
+    def __init__(self):
+        self.valid_keys = {'status','metadata','steps','misc'}
+        self.output = {
+            'status' : 'fail',
+            'metadata' : None,
+            'steps' : [],
+            'message' : None,
+            'misc' : []
+        }
     
     def updates(self,*args : tuple[str,str | dict | Exception]):
         for key,data in args:
